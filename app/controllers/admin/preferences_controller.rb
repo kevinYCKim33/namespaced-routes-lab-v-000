6c2bf634f69431b96c10681efb8383e5ac85516e
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-08 23:02:17 -0800

    preference edits and updates properly

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -9,6 +9,9 @@ class Admin::PreferencesController < ApplicationController
   end
 
   def update
+    binding.pry
+    @preference.update(preference_params)
+    redirect_to preferences_path
   end
 
   private
@@ -16,6 +19,8 @@ class Admin::PreferencesController < ApplicationController
       @preference = Preference.last
     end
 
-
+    def preference_params
+      params.require(:preference).permit(:artist_song_order, :song_sort_order, :allow_create_artists, :allow_create_songs)
+    end
 
 end
